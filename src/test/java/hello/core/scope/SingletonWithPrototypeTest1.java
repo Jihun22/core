f51f07183ebf01384c410975bf2b9a3a052182b1
YANGJIHUN <ik5334@naver.com>
2023-07-17 22:46:44 +0900

    싱글톤 빈과 함께 사용시 문제점

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -21,6 +22,35 @@ public class SingletonWithPrototypeTest1 {
         PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
         prototypeBean2.addCount();
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
+    }
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(Clientbean.class,PrototypeBean.class);
+
+       Clientbean clientbean1 = ac.getBean(Clientbean.class);
+       int count1 = clientbean1.logic();
+       assertThat(count1).isEqualTo(1);
+
+       Clientbean clientbean2 =ac.getBean(Clientbean.class);
+      int count2 =  clientbean2.logic();
+      assertThat(count2).isEqualTo(2);
+
+    }
+
+    @Scope("singleton")
+    static  class  Clientbean{
+        private final PrototypeBean prototypeBean; //생성시점에 주입
+
+        @Autowired
+        public Clientbean(PrototypeBean prototypeBean){
+            this.prototypeBean = prototypeBean;
+        }
+        public int logic() {
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
     }
 
     @Scope("prototype")
