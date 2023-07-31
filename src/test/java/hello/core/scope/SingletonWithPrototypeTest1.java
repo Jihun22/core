26155fb8960d840deea0c79d2a919a4d175c8d5c
YANGJIHUN <ik5334@naver.com>
2023-07-31 22:36:28 +0900

    Provider.get()  새로운 프로토타입 빈 생성 자바 표준 , DL정도의 기능만 제공

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,12 +1,14 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,19 +36,20 @@ public class SingletonWithPrototypeTest1 {
 
        Clientbean clientbean2 =ac.getBean(Clientbean.class);
       int count2 =  clientbean2.logic();
-      assertThat(count2).isEqualTo(2);
+      assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
     static  class  Clientbean{
-        private final PrototypeBean prototypeBean; //생성시점에 주입
+
 
         @Autowired
-        public Clientbean(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+
+
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
